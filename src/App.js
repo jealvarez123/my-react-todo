ed224eaaa41a307d9cb3021ee702e032d1db31e3
jealvarez123 <jealvarez123@gmail.com>
2018-05-03 09:43:27 -0500

    added the commenting to the todo list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+
+import MyRoutes from './config/routes';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+      { MyRoutes }
       </div>
     );
   }
---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import TodoModel from '../models/Todo'
+import Todos from '../components/Todos'
+import CreateTodoForm from '../components/CreateTodoForm.js'
+
+class TodosContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+  componentDidMount(){
+    this.fetchData()
+  }
+  fetchData(){
+    TodoModel.all().then( (res) => {
+        this.setState ({
+          todos: res.data.todos,
+          todo: ''
+        })
+      })
+    }
+    createTodo(todo) {
+    let newTodo = {
+        body: todo,
+        completed: false
+    }
+    TodoModel.create(newTodo).then((res) => {
+        let todos = this.state.todos
+        let newTodos = todos.push(res.data)
+        this.setState({newTodos})
+    })
+}
+render(){
+  return (
+    <div className="todosComponent">
+      <Todos
+        todos={this.state.todos} />
+      <CreateTodoForm
+        createTodo={ this.createTodo }
+        />
+    </div>
+  )
+}
+  }
+
+  export default TodosContainer
